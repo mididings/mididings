4c9adf2355682c69e92a9c3eca48687641cfc61c
George Rawlinson <george@rawlinson.net.nz>
2022-12-28 21:21:06 +1300

    feat: implement misc.formatargspec
    The inspect.formatargspec() function was dropped in Python 3.11, but it
    is required internally. A wrapper function was implemented with code
    from Cyril Jouve.

--- mididings/misc.py
+++ mididings/misc.py
@@ -78,6 +78,40 @@ def getargspec(f):
         _argspec_cache[f] = r
         return r
 
+"""
+The inspect.formatargspec() function was dropped in Python 3.11 but we need
+it when constructing signature changing decorators based on result of
+inspect.getargspec() or inspect.getfullargspec(). The code here implements
+inspect.formatargspec() based on Parameter and Signature from inspect module,
+which were added in Python 3.6. Thanks to Cyril Jouve for the implementation.
+"""
+def formatargspec(args, varargs=None, varkw=None, defaults=None,
+                  kwonlyargs=(), kwonlydefaults={}, annotations={}):
+    if kwonlydefaults is None:
+        kwonlydefaults = {}
+    ndefaults = len(defaults) if defaults else 0
+    parameters = [
+        inspect.Parameter(
+            arg,
+            inspect.Parameter.POSITIONAL_OR_KEYWORD,
+            default=defaults[i] if i >= 0 else inspect.Parameter.empty,
+            annotation=annotations.get(arg, inspect.Parameter.empty),
+        ) for i, arg in enumerate(args, ndefaults - len(args))
+    ]
+    if varargs:
+        parameters.append(inspect.Parameter(varargs, inspect.Parameter.VAR_POSITIONAL))
+    parameters.extend(
+        inspect.Parameter(
+            kwonlyarg,
+            inspect.Parameter.KEYWORD_ONLY,
+            default=kwonlydefaults.get(kwonlyarg, inspect.Parameter.empty),
+            annotation=annotations.get(kwonlyarg, inspect.Parameter.empty),
+        ) for kwonlyarg in kwonlyargs
+    )
+    if varkw:
+        parameters.append(inspect.Parameter(varkw, inspect.Parameter.VAR_KEYWORD))
+    return_annotation = annotations.get('return', inspect.Signature.empty)
+    return str(inspect.Signature(parameters, return_annotation=return_annotation))
 
 class deprecated(object):
     def __init__(self, replacement=None):
