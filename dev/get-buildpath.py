5537ede0aaf396c5983635c5a90e854739216c74
George Rawlinson <george@rawlinson.net.nz>
2023-01-19 20:38:54 +1300

    refactor: simplify buildpath script

--- dev/get-buildpath.py
+++ dev/get-buildpath.py
@@ -5,14 +5,13 @@ import os
 
 # python -m build creates temporary directories when building the package,
 # we can use these temporary directories instead of installing the package
-# afterwards.
+# afterwards. the format of this path is as follows:
+# $(pwd)/build/lib.linux-x86_64-cpython-310
 
 cwd = os.getcwd()
-system = platform.system().lower() # linux
-arch = platform.machine() # x86_64
-impl = platform.python_implementation().lower() # cpython
-pyver = "".join(map(str, platform.python_version_tuple()[:2])) # 310
+system = platform.system().lower()
+arch = platform.machine()
+impl = platform.python_implementation().lower()
+pyver = "".join(map(str, platform.python_version_tuple()[:2]))
 
-path = f"{cwd}/build/lib.{system}-{arch}-{impl}-{pyver}" # $PWD/build/lib.linux-x86_64-cpython-310
-
-print(path)
+print(f"{cwd}/build/lib.{system}-{arch}-{impl}-{pyver}")
